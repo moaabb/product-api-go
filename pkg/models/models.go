d79b63bc3c89b6171f3becabb3d26d260d41d63a
Moab <76390290+moaabb@users.noreply.github.com>
2023-04-11 18:58:19 +0000

    fixing db model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -4,9 +4,8 @@ import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
-	ID    int  `json:"id,omitempty" gorm:"primaryKey"`
 	Name  string `json:"name"`
 	SKU   string `json:"sku"`
-	Stock int  `json:"stock"`
-	Price int  `json:"price"`
+	Stock int    `json:"stock"`
+	Price int    `json:"price"`
 }
